b026cc695536b046054a2bfc7439bfbe2760dd16
kokoro-kanade <kanadekokoro96731@gmail.com>
2023-05-09 18:04:37 +0900

    mailデータベース作成

--- server/team2/mail/models.py
+++ server/team2/mail/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Mail(models.Model):
+    ThreadId = models.CharField(max_length=100) # メールスレッドid
+    ExpiryTime = models.DateTimeField() # 締め切り日時
