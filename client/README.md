12bc9c2d5e3253efd0577992e0c71dd534c59d6e
hl_tang <hltang@protonmail.com>
2023-05-16 14:49:36 +0900

    first commit frontend

---
+++ client/README.md
@@ -0,0 +1,56 @@
+# Gmail Clone (gmail-clone)
+
+### Tutorial series on how to build this
+
+If you'd like a step by step guide on how to build this just **CLICK THE IMAGE BELOW**
+
+[![GO TO JOHN WEEKS DEV TUTORIAL VIDEOS](https://user-images.githubusercontent.com/108229029/208315507-39a5cac2-47bc-4559-95db-db0fab56bc01.jpeg)](https://www.youtube.com/watch?v=KE5sQiHRZQg)
+
+Come and check out my YOUTUBE channel for lots more tutorials -> https://www.youtube.com/@johnweeksdev
+
+**LIKE**, **SUBSCRIBE**, and **SMASH THE NOTIFICATION BELL**!!!
+
+## App Setup
+
+Clone the repository
+```
+git clone https://github.com/John-Weeks-Dev/gmail-clone.git
+```
+
+Now go to https://console.cloud.google.com/
+
+Generate an API KEY.
+
+Add your new API KEY to the script inside **src/main.js**
+
+
+![Screenshot 2022-12-19 at 14 33 10](https://user-images.githubusercontent.com/108229029/208371968-8a66bbed-d157-4ab3-927f-cc573e6f9aaf.png)
+
+And **backend/index.js**
+
+![Screenshot 2022-12-19 at 14 33 32](https://user-images.githubusercontent.com/108229029/208372044-a867263c-b3aa-4575-ba44-8caa8f2ba8ba.png)
+
+Setup Firebase (firestore)
+
+Add the details to **src/firebase-init.js**
+
+Now run this command to start the project 
+```
+npm i
+
+npm run serve
+```
+
+And to start the backend
+```
+cd backend
+
+npm i
+
+npm run watch
+```
+
+You should be good to go!
+
+# Application Images
+<img width="1219" alt="Screenshot 2022-12-19 at 14 42 47" src="https://user-images.githubusercontent.com/108229029/208373071-7517e1cd-05be-4a31-964e-0f391f5ca30f.png">
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="HomeView" class="w-full bg-white ml-2 rounded-t-xl h-full shadow-sm">
+
+    <div class="border-b">
+      <div class="flex items-center justify-between px-4 my-3">
+        <IconComponent
+          @click="deleteSelected"
+          class="-m-2 -ml-2.5"
+          iconString="trash"
+          :iconSize="19"
+          iconColor="#636363"
+          text="Delete selected"
+          hoverColor="hover:bg-gray-100"
+        />
+        <div class="text-xs text-gray-500">1-50 of 150</div>
+      </div>
+    </div>
+
+    <div>
+      <MessageRow
+        from="aaa@bbb.com"
+        subject="Test row 1"
+        body="aaaaaa"
+        time="2023/5/16 09:46"
+      />
+    </div>
+    <div>
+      <MessageRow
+        from="cccc@ddd.com"
+        subject="Test row 2"
+        body="dfsgdfgfdgsdfg"
+        time="2023/5/16 09:46"
+      />
+    </div>
+
+    <div v-for="email in userStore.emails" :key="email">
+        <MessageRow
+          :id="email.id"
+          :from="email.firstName + ' ' + email.lastName"
+          :subject="email.subject"
+          :body="email.body"
+          :time="email.createdAt"
+          :hasViewed="email.hasViewed"
+          @selectedId="selectedId"
+        />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { onMounted } from 'vue'
+  import MessageRow from "@/components/MessageRow.vue";
+  import IconComponent from '@/components/IconComponent.vue';
+  import { useUserStore } from "@/store/user-store";
+  const userStore = useUserStore()
+
+  let emailsToDelete = []
+
+  onMounted(() => {
+    userStore.getEmailsByEmailAddress()
+  })
+
+  const selectedId = (e) => {
+    if (!emailsToDelete.length) {
+      emailsToDelete.push(e.id)
+    } else if (e.bool && !emailsToDelete.includes(e.id)) {
+      emailsToDelete.push(e.id)
+    } else if (!e.bool && emailsToDelete.includes(e.id)) {
+      const index = emailsToDelete.indexOf(e.id);
+      if (index > -1) { 
+        emailsToDelete.splice(index, 1); 
+      }
+    }
+  }
+
+  const deleteSelected = () => {
+    if (!emailsToDelete.length) return
+    
+    let res = confirm("Are you sure you want to delete the selected emails?");
+    if (res) { 
+      emailsToDelete.forEach(async id => {
+        await userStore.deleteEmail(id)
+      })
+
+      emailsToDelete = []
+    }
+  }
+
+</script>
