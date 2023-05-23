fe1774e704844985318a0f0b54900ebfd97b4f72
Hizume Naoki <hizumee@gmail.com>
2023-05-23 16:18:51 +0900

    add translate level

--- server/team2/translation/translate.py
+++ server/team2/translation/translate.py
@@ -4,8 +4,9 @@ from . import chatGptApi
 def translate(json_data):
     pre_dict = json.loads(json_data)
     pre_str = pre_dict["word"]
+    pre_level = pre_dict["level"]
     manager = chatGptApi.chatGptManager()
-    keigo_str_list = manager.translate(pre_str)
+    keigo_str_list = manager.translate(pre_str, level=pre_level)
     keigo_dict = {}
     keigo_dict["translated_words"] = keigo_str_list
     json_keigo = json.dumps(keigo_dict, ensure_ascii=False)
