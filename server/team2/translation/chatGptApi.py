05416326d3a90efa1f18c6e8cabcae1bb4c72276
Hizume Naoki <hizumee@gmail.com>
2023-05-23 14:04:25 +0900

    sendMail and chatGpt connect

--- server/team2/translation/chatGptApi.py
+++ server/team2/translation/chatGptApi.py
@@ -1,5 +1,6 @@
 import openai
 import json
+import re
 
 
 class chatGptManager:
@@ -10,20 +11,44 @@ class chatGptManager:
         openai.api_key = info["api_key"]
 
     def create_query(self, content, level=0):
-        return f'「{content}」をより丁寧な敬語に変換した文の例をいくつか挙げ、「、」で区切って回答してください'
+        if level == 0:
+            return f'「{content}」をローレベルな敬語に変換した文の例をいくつか挙げてください。'
+        elif level == 1:
+            return f'「{content}」をミドルレベルな敬語に変換した文の例をいくつか挙げてください。'
+        else:
+            return f'「{content}」をハイレベルな敬語に変換した文の例をいくつか挙げてください。'
+
+    
+    def parse_result(self, result: str):
+        if '->' in result:
+            result = result.split('->')[1]
+
+        replace_list = ["・", "「", "」", "'", '"', " "]
+        for replace in replace_list:
+            result = result.replace(replace, "")
+        return result
 
     def translate(self, content):
+        structure = "・「ans1」\n・「ans2」\n..."
+
         res = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "user",
-                    "content": self.create_query(content, level=0)
+                    "content": self.create_query(content, level=0) 
+                                + "\n回答は以下の形式に従ってください\n" 
+                                + structure,
                 },
             ],
         )
 
-        return res["choices"][0]["message"]["content"].split("、")
+        body = res["choices"][0]["message"]["content"]
+        results = body.split('\n')
+        if len(results) < 1:
+            return []
+        
+        return [self.parse_result(result) for result in results if result[0] == '・']
     
 
 if __name__ == '__main__':
