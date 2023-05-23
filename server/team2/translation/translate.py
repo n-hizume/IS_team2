e018e0cc17ed4092fe4b9d9183c388e65f6160fd
kokoro-kanade <kanadekokoro96731@gmail.com>
2023-05-23 14:31:02 +0900

    translate

--- server/team2/translation/translate.py
+++ server/team2/translation/translate.py
@@ -1,15 +1,12 @@
 import json
 import chatGptApi
 
-# TODO
-# jsonの仕様が決まったら変更
 def translate(json_data):
-    dict = json.loads(json_data)
-    pre_str = dict["str"]
+    pre_dict = json.loads(json_data)
+    pre_str = pre_dict["word"]
     manager = chatGptApi.chatGptManager()
-    keigo_str = manager.translate(pre_str)
+    keigo_str_list = manager.translate(pre_str)
     keigo_dict = {}
-    keigo_dict["str"] = keigo_str
+    keigo_dict["translated_words"] = keigo_str_list
     json_keigo = json.dumps(keigo_dict)
     return json_keigo
-
