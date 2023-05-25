6c3f8e2db3510e100ee85661baad256b2052a630
Hizume Naoki <hizumee@gmail.com>
2023-05-26 04:00:22 +0900

    google oauth2

--- client/src/main.js
+++ client/src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
-
 import vue3GoogleLogin from 'vue3-google-login'
 
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
@@ -17,8 +15,13 @@ const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.use(store)
-app.use(vue3GoogleLogin, {
-  clientId: '290280505278-mq7jf5gn8u5e2gssj3i27bf4r10v9o9k.apps.googleusercontent.com' // EG: 184413169503-jfhjjaskdfhdsakfhjsa.apps.googleusercontent.com
-})
+
+const options = {
+  clientId: '290280505278-jr662shaa29lke42ckon55fumjp8dn4b.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'slelect_account'
+}
+
+app.use(vue3GoogleLogin, options)
 
 app.mount('#app')
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,25 +1,66 @@
 <template>
     <div id="LoginView" class="grid h-screen place-items-center">
         <div>
-            <img width="279" src="img/GmailLogin.png" alt="">
-            <div class="flex justify-center">
-                <GoogleLogin :callback="callback"/>
+            <div class="login-btn" @click="login">
+                <img width="279" src="img/GmailLogin.png" alt="">
+                <div class="flex justify-center">
+                    Googleログイン
+                </div>
             </div>
+            <!-- <a href="" @click="useGoogleAuth" >Google でログイン</a> -->
         </div>
     </div>
 </template>
 
 <script setup>
-    import { onMounted } from '@vue/runtime-core'
-    import { useRouter } from 'vue-router';
+import { googleSdkLoaded } from "vue3-google-login";
+import axios from 'axios'
+import { useRouter } from "vue-router";
 
-    const router = useRouter()
+const router = useRouter();
 
-    onMounted(() => {
-        setTimeout(() => { router.push('/email') }, 250)
+const getToken = async (code) => {
+    axios.post('http://kuisteam2.pythonanywhere.com/mail/auth/', {
+        "code": code,
+        "redirect_url": "http://localhost:8082"
+    }).then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.log(err)
     })
 
-    const callback = async () => {
-        setTimeout(() => { router.push('/email') }, 250)
-    }
+}
+
+const login = () => {
+    googleSdkLoaded((google) => {
+        google.accounts.oauth2.initCodeClient({
+            client_id: '290280505278-jr662shaa29lke42ckon55fumjp8dn4b.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/gmail.modify',
+            callback: (response) => {
+                getToken(response.code);
+                router.push('email')
+            }
+        }).requestCode()
+
+    })
+
+}
+
+
+
 </script>
+
+<style lang="scss">
+
+.login-btn {
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 1px 1px 10px 1px #888888;
+}
+
+.login-btn:hover {
+    cursor: pointer;
+    box-shadow: 1px 1px 10px 1px #555555;
+}
+
+</style>
