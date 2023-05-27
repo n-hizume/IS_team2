7da9e56fa2bf8003848ecdde3ec72c48ee060645
saki <hirayama.saki.62n@st.kyoto-u.ac.jp>
2023-05-28 03:45:14 +0900

    add file

--- client/src/components/SettingInformation.vue
+++ client/src/components/SettingInformation.vue
@@ -1,37 +1,72 @@
 <template>
-    <div @click="showSettingInformation" class="
-    cursor-pointer text-zinc-600">
-        <Information :size="20"/>
+    <div @click="showSettingInformation"> 
+        <div class="cursor-pointer text-zinc-600">
+            <Information :size="20"/>
+        </div>
     </div>
 
     <div v-if="SettingInformationOpen" id="SettingInformationSection" class="
         absolute 
-        
         right-0 
         mr-9
-        
         rounded-2xl
         shadow-2xl 
         bg-white"
     >
-    <!-- <div class="flex items-center justify-between rounded-t-lg w-full text-sm px-3.5 py-2.5">
-        <CloseIcon class="cursor-pointer ml-auto mb-1" @click="SettingInformationOpen = false" :size="19" />
-    </div> -->
+        <div class="px-4 py-4">
+            <div class="flex text-zinc-600">
+                <ClockOutlineIcon :size="18" />
+                <h3 class="ml-2 text-sm font-semibold">通知設定</h3>
+            </div> 
+            <div class="ml-5 text-zinc-600" style="text-align: left;">   
+                <div class="date mt-3 cursor-pointer">
+                        <label for="alarm-date" class="custom block text-sm font-semibold"></label>
+                        <input type="date" id="alarm-date" v-model="alarmDate" class="rounded-full px-2 py-1">
+                </div>
+                <div class="flex items-center">
+                    <div class="time mt-3">
+                            <label for="alarm-time" class="block"></label>
+                            <input type="time" id="alarm-time" v-model="alarmTime" class="border rounded-full px-2 py-1">
+                    </div>
+                    <div class="mt-3 ml-2">
+                            <button @click="saveAlarm" class="text-sm bg-zinc-600 hover:bg-primary-400 text-white font-bold py-1.5 px-2 rounded-full">
+                            保存
+                            </button>
+                    </div>
+                </div>    
+            </div>  
+            <div class="mt-5 flex text-zinc-600 cursor-pointer">
+                <TrashCanOutline :size="18" class="mt-1" />
+                <button class="ml-0.5 hover:bg-primary-400 py-1 px-1 rounded-full">
+                    <h3 class="text-sm font-semibold">メールを削除する</h3>
+                </button>    
+            </div>
+        </div>
     </div>
+    
+
         
 </template>
 
 <script setup>
+import ClockOutlineIcon from "vue-material-design-icons/ClockOutline.vue";
+
 import { ref } from "vue";
 const SettingInformationOpen = ref(false);
 const showSettingInformation = () => {
     SettingInformationOpen.value = !SettingInformationOpen.value;
 }
 
-
-
 import Information from "vue-material-design-icons/InformationOutline.vue";
-// import CloseIcon from "vue-material-design-icons/Close.vue";
+import TrashCanOutline from "vue-material-design-icons/TrashCanOutline.vue";
+
+const alarmDate = ref("");
+const alarmTime = ref("");
+
+const saveAlarm = () => {
+  console.log("保存されたアラーム日付:", alarmDate.value);
+  console.log("保存されたアラーム時刻:", alarmTime.value);
+}
 
 </script>
 
@@ -39,6 +74,20 @@ import Information from "vue-material-design-icons/InformationOutline.vue";
 #SettingInformationSection {
 overflow: hidden;
 width: 200px;
-height: 250px;
+height: 196px;
+
 }
+
+.custom{
+   -webkit-tap-highlight-color:rgba(0,0,0,0);
+}
+
+*:focus {
+outline: none;
+}
+
+
+
+
+
 </style>
