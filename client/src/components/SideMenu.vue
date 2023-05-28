7c39780932ee8d412b4d6152ab59bf7d4758bcb3
hl_tang <hltang@protonmail.com>
2023-05-28 15:31:56 +0900

    add transformKeigo function

--- client/src/components/SideMenu.vue
+++ client/src/components/SideMenu.vue
@@ -2,12 +2,11 @@
     <div class="side-menu-bar flex">
       <div class="flex  w-full">
         <div class="side-menu">
-          <div @click="newMessageOpen = true" class="
+          <div @click="newbodyOpen = true" class="
               flex
               items-center
               justify-center
               bg-primary-100 w-52 
-              text-primary-600
               
               h-20
               mt-10
@@ -17,7 +16,7 @@
               cursor-pointer
             ">
             <PencilOutlineIcon :size="27" class="mr-2" />
-            <span class="text-sm font-bold ">メール作成</span>
+            <span class="text-sm font-bold text-primary-600">メール作成</span>
           </div>
 
           <router-link to="/email">
@@ -34,11 +33,9 @@
               ">
 
 
-              <div class="flex items-center text-primary-600"> 
-                <div class="mr-1">
-                  <InboxIcon :size="17" />
-                </div>  
-                <div class="text-sm pl-4 font-bold text-align-left">受信箱</div>
+              <div class="flex items-center"> 
+                <InboxIcon :size="17" />
+                <div class="text-sm pl-4 font-bold text-primary-600 text-align-left">受信箱</div>
               </div>
             </div>
           </router-link>
@@ -57,83 +54,82 @@
               rounded-3xl
               cursor-pointer
             ">
-            <div class="flex items-center text-primary-600">
+            <div class="flex items-center">
              <FileOutlineIcon :size="17" />
-              <div class="text-sm pl-5 font-bold">下書き</div>
+              <div class="text-sm pl-5 font-bold text-primary-600">下書き</div>
             </div>
           </div>
 
-          <div class="flex justify-center mt-2 ml-5 px-8 py-3  bg-primary-200 hover:bg-primary-500 w-52 h-21 mr-5 rounded-3xl cursor-pointer">
-            <div class="flex items-center text-primary-600">
-              <div class="ml-2">
-                <SendOutlineIcon :size="17" />
-              </div>  
-              <div class="text-sm pl-3 font-bold">送信済み</div>
+          <div @click="movesendScreen"
+            class="flex justify-center mt-2 ml-5 px-8 py-3  bg-primary-200 hover:bg-primary-500 w-52 h-21 mr-5 rounded-3xl cursor-pointer">
+            <div class="flex items-center">
+              <SendOutlineIcon :size="17" />
+              <div class="text-sm pl-4 font-bold text-primary-600">送信済み</div>
             </div>
           </div>
 
           <div
             class="flex justify-center mt-2 ml-5 mr-5 px-8 py-3  bg-primary-200 hover:bg-primary-500 w-52 h-21 rounded-3xl cursor-pointer">
-            <div class="flex items-center text-primary-600">
-              <div class="mr-1">
-                <TrashCanOutline :size="17" />
-              </div>  
-              <div class="text-sm pl-4 font-bold">ゴミ箱</div>
+            <div class="flex items-center">
+              <TrashCanOutline :size="17" />
+              <div class="text-sm pl-4 font-bold text-primary-600">ゴミ箱</div>
             </div>
           </div>
 
           <div @click="movealarmScreen" 
             class="flex justify-center mt-2 ml-5 mr-5 px-8 py-3  bg-primary-200 hover:bg-primary-500 w-52 h-21 rounded-3xl cursor-pointer">
-            <div class="flex items-center text-primary-600">
-              <div class="mr-3">
-                <ClockOutlineIcon :size="17" />
-              </div>  
-              <div class="text-sm pl-4 mr-1 font-bold">通知</div>
+            <div class="flex items-center">
+              <ClockOutlineIcon :size="17" />
+              <div class="text-sm pl-4 font-bold text-primary-600">通知</div>
             </div>
           </div>
-          <div class="mt-96 ml-5 text-zinc-600">
+          <div class="mt-96 ml-5">
             <Cog :size="30" />
           </div>
         </div>
       </div>   
     </div>
 
-    <div v-if="newMessageOpen" id="NewMessageSection" class="
-      absolute 
-      bottom-11 
-      right-0 
-      mr-20 
-      rounded-2xl
-      shadow-2xl 
-      bg-white
-    ">
-    <div class="flex items-center justify-between rounded-t-lg w-full text-sm px-3.5 py-2.5 bg-gray-200">
-      <div>New message</div>
-      <CloseIcon class="cursor-pointer" @click="newMessageOpen = false" :size="19" />
-    </div>
-
-    <div>
-      <div class="relative flex items-center px-3.5 py-2">
-        <div class="text-sm text-gray-700">To</div>
-        <input v-model="toEmail" class="w-full h-6 border-transparent border-none focus:ring-0 outline-none" type="text">
-        <div class="absolute border-b w-[calc(100%-30px)] bottom-0"></div>
+    <div
+      v-if="newbodyOpen" 
+      id="NewbodySection" 
+      class="
+        absolute 
+        bottom-11 
+        right-0 
+        mr-20 
+        rounded-2xl
+        shadow-2xl 
+        bg-white
+      "
+    >
+      <div class="flex items-center justify-between rounded-t-lg w-full text-sm px-3.5 py-2.5 bg-gray-200">
+        <div>New body</div>
+        <CloseIcon class="cursor-pointer" @click="newbodyOpen = false" :size="19" />
       </div>
-      <div class="relative flex items-center px-3.5 py-2">
-        <div class="text-sm text-gray-700">Subject</div>
-        <input v-model="subject" class="w-full h-6 border-transparent border-none focus:ring-0 outline-none" type="text">
-        <div class="absolute border-b w-[calc(100%-30px)] bottom-0"></div>
-      </div>
-    </div>
 
-    <div class="m-1">
-      <textarea v-model="body" style="resize:none" class="
-          w-full 
-          border-transparent 
-          border-none 
-          focus:ring-0 
-          outline-none
-        " rows="14"></textarea>
-    </div>
+      <div>
+        <div class="relative flex items-center px-3.5 py-2">
+          <div class="text-sm text-gray-700">To</div>
+          <input v-model="toEmail" class="w-full h-6 border-transparent border-none focus:ring-0 outline-none" type="text">
+          <div class="absolute border-b w-[calc(100%-30px)] bottom-0"></div>
+        </div>
+        <div class="relative flex items-center px-3.5 py-2">
+          <div class="text-sm text-gray-700">Subject</div>
+          <input v-model="subject" class="w-full h-6 border-transparent border-none focus:ring-0 outline-none" type="text">
+          <div class="absolute border-b w-[calc(100%-30px)] bottom-0"></div>
+        </div>
+      </div>
+
+      <div class="m-1">
+        <textarea v-model="body" placeholder="please input content" style="resize:none" class="
+            w-full 
+            border-transparent 
+            border-none 
+            focus:ring-0 
+            outline-none
+          " rows="14"></textarea>
+      </div>
 
     <div class="p-4 mt-5 flex">
         <button @click="sendEmail" class="send 
@@ -145,20 +141,35 @@
             rounded-full
             
             
-            
-            
-            
-            
-            
-            
             justify-center
             
-            ">
-            <div class="mr-2">
-                <SendOutlineIcon :size="17" />
-            </div>
-            送信
+            "
+        >
+          <div class="mr-2">
+              <SendOutlineIcon :size="17" />
+          </div>
+          送信
         </button>
+
+        <div class="button-space"></div> <!-- 隙間 -->
+
+        <!-- 敬語変換 -->
+        <button 
+        @click="transformKeigo" 
+        class="
+          bg-green-700 
+          hover:bg-green-600 
+          text-white 
+          text-sm 
+          font-bold 
+          py-2 
+          px-4 
+          rounded-full
+        "
+        >
+          敬語変換 
+        </button>
+
     </div>
 
   </div>
@@ -176,10 +187,13 @@ import FileOutlineIcon from "vue-material-design-icons/FileOutline.vue";
 import CloseIcon from "vue-material-design-icons/Close.vue";
 import Cog from "vue-material-design-icons/CogOutline.vue";
 
-let newMessageOpen = ref(false)
+import axios from 'axios';
+
+let newbodyOpen = ref(false)
 let toEmail = ref('')
 let subject = ref('')
 let body = ref('')
+// const body = ref('')
 
 import { useRouter } from "vue-router";
 
@@ -188,9 +202,39 @@ const movealarmScreen = () => {
   router.push('/alarm')
 };
 
-// const movesendScreen = () => {
-//   router.push('/send')
-// };
+const movesendScreen = () => {
+  router.push('/send')
+};
+
+
+/* const sendEmail = () => {
+  // 发起POST请求
+  axios
+    .post('http://127.0.0.1:8000/', body.value)
+    .then((response) => {
+      // 请求成功回调
+      console.log('POST success');
+      console.log('response body:', response.data);
+    })
+    .catch((error) => {
+      // 请求失败回调
+      console.error('POST error', error);
+    });
+}; */
+
+const transformKeigo = () => {
+  axios
+    .post('http://127.0.0.1:8000/translation/gpt/', { "word": body, "level": 0 })
+    .then((response) => {
+      // body = response.data["translated_words"][0];
+      body.value = response.data.translated_words.join('\n');
+      // const reslist = response.data["translated_words"]
+      // const num = reslist.length()
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 
 </script>
@@ -285,10 +329,13 @@ height:35px;
 box-shadow: 0px 0px 12px 5px rgba(0, 0, 0, .08);
 }
 
+.button-space {
+  margin-right: 20px; /* 调整空隙的大小 */
+}
 
 
 
-#NewMessageSection {
+#NewbodySection {
 overflow: hidden;
 width: 560px;
 height: 570px;
