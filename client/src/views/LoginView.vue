abfb08ae74adfe11168f1a7d96ff2e475186aa53
hl_tang <hltang@protonmail.com>
2023-05-29 22:30:18 +0900

    add loading hint

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,4 +1,12 @@
 <template>
+    <div>
+      <el-progress v-if="isGetMail"
+        :percentage="100"
+        status="success"
+        :indeterminate="true"
+        :duration="5"
+      />
+    </div>
     <div id="LoginView" class="grid h-screen place-items-center">
         <div class="container">
             <div class="image-container">
@@ -7,8 +15,10 @@
             <div class="font-semibold text-primary-600 login-btn m-5 mx-auto" @click="login">
                 Google ログイン
             </div>
+            
         </div>
     </div>
+    
 </template>
 
 
@@ -17,8 +27,11 @@ import { googleSdkLoaded } from "vue3-google-login";
 import { storeToken } from '@/apis/mail'
 import { storeMails } from '@/utils/index'
 import { useRouter } from "vue-router";
+import { ref } from 'vue'
 
 const router = useRouter();
+
+var isGetMail = ref(false)
 
 const login = () => {
     googleSdkLoaded((google) => {
@@ -26,8 +39,11 @@ const login = () => {
             client_id: '290280505278-jr662shaa29lke42ckon55fumjp8dn4b.apps.googleusercontent.com',
             scope: 'https://www.googleapis.com/auth/gmail.modify',
             callback: async (response) => {
+                isGetMail.value = true;
                 await storeToken(response.code);
                 await storeMails()
+                // isGetMail.value = false;
+                
                 router.push({name:'email'})
             }
         }).requestCode();
