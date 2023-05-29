b894299ebd34ee4a4a89d663db9f609bea54f4be
Hizume Naoki <hizumee@gmail.com>
2023-05-30 05:35:12 +0900

    gpt flag

--- client/src/apis/gpt.js
+++ client/src/apis/gpt.js
@@ -1,21 +1,23 @@
-// import { BASE_URL } from "./mail"
-// import axios from 'axios'
+import { BASE_URL } from "./mail"
+import axios from 'axios'
 
 export const translateByGpt = async (text, level=0) => {
-    // const res =  await axios.post(BASE_URL+'/translation/gpt/', {
-    //     "word": text,
-    //     "level": level
-    // })
-    // return res.data["translated_words"]
-    
-    console.log(text, level)
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    return [
-        text +"のダミーアンサーひとつめです", 
-        text +"のダミーアンサーふたつめです", 
-        text +"のダミーアンサーみっつめです",
-        text +"のダミーアンサーよっつめです",
-        text +"のダミーアンサーいつつめです",
-        text +"のダミーアンサーむっつめです",
-    ]
+    const isProd = false;
+    if (isProd) {
+        const res =  await axios.post(BASE_URL+'/translation/gpt/', {
+            "word": text,
+            "level": level
+        })
+        return res.data["translated_words"]
+    } else {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        return [
+            text +"のダミーアンサーひとつめです", 
+            text +"のダミーアンサーふたつめです", 
+            text +"のダミーアンサーみっつめです",
+            text +"のダミーアンサーよっつめです",
+            text +"のダミーアンサーいつつめです",
+            text +"のダミーアンサーむっつめです",
+        ]
+    }
 }
